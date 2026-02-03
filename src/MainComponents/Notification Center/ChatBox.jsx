060694541af69e006d22002fba9136ce76ce5b5d
akshatmikki <akshat.singhal@thynkweb.com>
2026-02-03 10:22:43 +0530

    changes

--- src/MainComponents/Notification Center/ChatBox.jsx
+++ src/MainComponents/Notification Center/ChatBox.jsx
@@ -54,7 +54,6 @@ const ChatBox = ({ remarks = [], onSend, status, context, assetData }) => {
 
   const chatEndRef = useRef(null);
 
-  const chatEndRef = useRef(null);
 
   const handleSend = async () => {
     if (context === "asset") {
